0f7eb968d292fe70287a5462d5239070b8d65a15
montrain <secret931230@gmail.com>
2026-01-07 15:30:01 +0800

    0107test002

--- server.js
+++ server.js
@@ -11,3 +11,20 @@ server.get("/about", (req,res)=>{
 })
 
 server.listen(8080)
+
+
+
+// test scroll effect code
+<script>
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".portfolio-container", {
+        x: "-500px", // 向左移動的距離
+        scrollTrigger: {
+            trigger: ".portfolio", // 當滾動到這個區塊時觸發
+            start: "top bottom",    // 當作品集頂部進入螢幕底部時開始
+            end: "bottom top",      // 當作品集底部離開螢幕頂部時結束
+            scrub: 1                // 讓動畫跟隨滑鼠滾動（1秒的緩衝效果）
+        }
+    });
+ </script>
